ec09be9836ab6e0fb8472a9947739da475d54773
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-08-02 20:22:55 +0530

    Added achievement :sparkle: Isn't that quite a one?

---
+++ Source/Commands/Edit-Text/Achievement.ts
@@ -0,0 +1,44 @@
+import BaseSlashCommand from "../../Base/BaseSlashCommand";
+import PhotoGenieClient from "../../Base/Client";
+import { CommandInteraction, MessageAttachment } from "discord.js";
+import { createCanvas, loadImage } from "canvas";
+import { join } from "path";
+
+export default class AchievementCommand extends BaseSlashCommand {
+	constructor(client: PhotoGenieClient) {
+		super(client, {
+			name: "achievement",
+			description: "Gets you a nice achievement",
+			options: [
+				{
+					name: "text",
+					description: "The achievement text",
+					type: "STRING",
+					required: true,
+				},
+			],
+		});
+	}
+
+	public async run(interaction: CommandInteraction) {
+		const text = interaction.options.getString("text", true);
+
+		const base = await loadImage(join(__dirname, "../../../Assets/Images/Achievement.png"));
+		const canvas = createCanvas(base.width, base.height);
+		const ctx = canvas.getContext("2d");
+
+		ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
+
+		ctx.translate(120, 60);
+		ctx.font = "24px 'Arial'";
+		ctx.fillStyle = "white";
+
+		ctx.fillText(text, 10, 22, 330);
+
+		return interaction.editReply({
+			files: [
+				new MessageAttachment(canvas.toBuffer("image/png"), "achievement.png"),
+			],
+		});
+	}
+}
